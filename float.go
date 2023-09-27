44ba8b2e233409787e86900957a3fc935f3afe13
Eric <eccisketchi@gmail.com>
2023-09-26 22:19:32 -0400

    prepub

--- float.go
+++ float.go
@@ -1,4 +1,4 @@
-package accurate
+package float
 
 import (
 	"log"
@@ -6,7 +6,7 @@ import (
 )
 
 /*
-A Float represents a signed floating point number that can do accurate calculations, meaning there are no inaccuracies when storing a value, unlike with traditional floats in computer science. An empty Float should not be considered a valid value and can not be used in artihmetic.
+An Float represents a signed floating point number that can do accurate calculations, meaning there are no inaccuracies when storing a value, unlike with traditional floats in computer science. An empty Float should not be considered a valid value and can not be used in artihmetic.
 
 Operations always take pointer arguments (*Float) rather than Float values, and each unique Float value requires its own unique *Float pointer. To "copy" a Float value, an existing (or newly allocated) Float must be set to a new value using the Float.Set method; shallow copies of Float are not supported and may lead to errors.
 
@@ -39,6 +39,119 @@ func (x *Float) Set(v string) {
 	x.SubOnePrecision = decSpots
 	x.NonDecRep = (ndc)
 
+}
+func Maxi(a *Float, b *Float) *Float {
+	if b.Cmp(a) == 1 {
+		return b
+	}
+	return a
+}
+func Mini(a *Float, b *Float) *Float {
+	if b.Cmp(a) == -1 {
+		return b
+	}
+	return a
+}
+func RoundTo(a *Float, d uint) *Float {
+	v := a.Value
+	origSuf := ""
+	origPref := ""
+	var sufAr []uint8
+	var prefAr []uint8
+	var tdp bool = false
+	for i := len(v) - 1; i >= 0; i-- {
+		if string(v[i]) == "." {
+			tdp = true
+		} else {
+			tempuint, _ := strconv.Atoi(string(v[i]))
+
+			if tdp == false {
+				origSuf = string(v[i]) + origSuf
+				sufAr = append([]uint8{uint8(tempuint)}, sufAr...)
+			} else {
+				origPref = string(v[i]) + origPref
+				prefAr = append([]uint8{uint8(tempuint)}, prefAr...)
+			}
+		}
+	}
+	log.Println(prefAr, sufAr, origPref, origSuf)
+	if d >= uint(len(sufAr)) {
+		return NewFloat(origPref + "." + origSuf)
+	}
+	if d == 0 {
+		if string(origSuf[0]) > "4" {
+
+			var carryover uint8 = 1
+			for i := len(prefAr) - 1; i >= 0 && carryover == 1; i-- {
+				prefAr[i]++
+				carryover = 0
+				if prefAr[i] > 9 {
+					prefAr[i] = 0
+					carryover = 1
+				}
+			}
+			if carryover == 1 {
+				prefAr = append([]uint8{1}, prefAr...)
+			}
+			origPref = ""
+			for i := 0; i < len(prefAr); i++ {
+				origPref += strconv.FormatUint(uint64(prefAr[i]), 10)
+			}
+			return NewFloat(origPref + ".0")
+		} else {
+			return NewFloat(origPref + ".0")
+		}
+	} else {
+		if string(origSuf[int(d)]) > "4" {
+			var carryover uint8 = 1
+			for i := int(d) - 1; i >= 0 && carryover == 1; i-- {
+				sufAr[i]++
+				carryover = 0
+				if sufAr[i] > 9 {
+					sufAr[i] = 0
+					carryover = 1
+				}
+			}
+			if carryover == 1 {
+				origSuf = "0"
+				carryover = 1
+				for i := len(prefAr) - 1; i >= 0 && carryover == 1; i-- {
+					prefAr[i]++
+					carryover = 0
+					if prefAr[i] > 9 {
+						prefAr[i] = 0
+						carryover = 1
+					}
+				}
+				if carryover == 1 {
+					prefAr = append([]uint8{1}, prefAr...)
+				}
+				origPref = ""
+				for i := 0; i < len(prefAr); i++ {
+					origPref += strconv.FormatUint(uint64(prefAr[i]), 10)
+				}
+			} else {
+				tempStr := ""
+				for i := 0; i < len(sufAr)-1; i++ {
+					tempStr += strconv.FormatUint(uint64(sufAr[i]), 10)
+				}
+				origSuf = tempStr
+			}
+		}
+	}
+	for uint(len(origSuf)) > d {
+		origSuf = origSuf[:len(origSuf)-1]
+	}
+	for len(origSuf) > 1 && string(origSuf[len(origSuf)-1]) == "0" {
+		origSuf = origSuf[:len(origSuf)-1]
+	}
+	for len(origPref) > 1 && string(origPref[0]) == "0" {
+		origPref = origPref[1:]
+	}
+
+	var retAF *Float = NewFloat(origPref + "." + origSuf)
+
+	return retAF
 }
 
 // NewFloat allocates and returns a pointer to a new Float set to v
@@ -79,7 +192,7 @@ func (a *Float) Div(b *Float, d uint) *Float {
 
 	decimalPlaces := d
 	if d == 0 {
-		panic("Must have atleast one place after the decimal when dividing. Floats always have a decimal point.")
+		decimalPlaces = 2
 	}
 
 	var negCount uint8 = 0
@@ -127,6 +240,34 @@ func (a *Float) Div(b *Float, d uint) *Float {
 	if len(sufStr) < int(decimalPlaces)+1 {
 		log.Println("TOO SHORT SUFFIX")
 	}
+	if d == 0 {
+		if string(sufStr[0]) > "4" {
+			prefAr := make([]uint8, len(prefStr))
+			for i := 0; i < len(prefStr); i++ {
+				tempuint, _ := strconv.Atoi(string(prefStr[i]))
+				prefAr[i] = uint8(tempuint)
+			}
+			var carryover uint8 = 1
+			for i := len(prefAr) - 1; i >= 0 && carryover == 1; i-- {
+				prefAr[i]++
+				carryover = 0
+				if prefAr[i] > 9 {
+					prefAr[i] = 0
+					carryover = 1
+				}
+			}
+			if carryover == 1 {
+				prefAr = append([]uint8{1}, prefAr...)
+			}
+			prefStr = ""
+			for i := 0; i < len(prefAr); i++ {
+				prefStr += strconv.FormatUint(uint64(prefAr[i]), 10)
+			}
+			return NewFloat(prefStr + ".0")
+		} else {
+			return NewFloat(prefStr + ".0")
+		}
+	}
 	sufAr := make([]uint8, len(sufStr))
 	for i := 0; i < len(sufStr); i++ {
 		tempuint, _ := strconv.Atoi(string(sufStr[i]))
@@ -136,7 +277,7 @@ func (a *Float) Div(b *Float, d uint) *Float {
 
 	} else if len(sufStr) > 0 && len(sufStr) > (int(decimalPlaces)) && string(sufStr[int(decimalPlaces)]) > "4" {
 		var carryover uint8 = 1
-		for i := len(sufAr) - 2; i >= 0 && carryover == 1; i-- {
+		for i := decimalPlaces - 1; i >= 0 && carryover == 1; i-- {
 
 			sufAr[i]++
 			carryover = 0
@@ -192,7 +333,9 @@ func (a *Float) Div(b *Float, d uint) *Float {
 	if negCount == 1 {
 		prefStr = "-" + prefStr
 	}
-
+	for uint(len(sufStr)) > d && d > 0 {
+		sufStr = sufStr[:len(sufStr)-1]
+	}
 	var retAF *Float = NewFloat(prefStr + "." + sufStr)
 
 	return retAF
